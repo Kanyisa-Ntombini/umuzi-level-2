b709d0206cb11b299d62105119dcd4a34b3596b1
Kanyisa-Ntombini <kanyisa.ntombini@gmail.com>
2022-04-09 20:51:05 +0200

    fixed the loop. still need to decided to use while or for loop

--- task2-3.js
+++ task2-3.js
@@ -1,9 +1,13 @@
 const columns = arr => {
-  for (let i=0; i<7; i++) {
-    let word = arr[i];
-    console.log(word);
+  let printWord = '';
+  for (let i=0; i<10; i++) {
+    printWord = '';
+    for (let word of arr) {
+      //printWord += word[i];
+      printWord = word[i] ? printWord + word[i] + ' ': printWord + '  ';
+    }
+    console.log(printWord);
   }
-  
 }
 
 columns(["Write","good","code","every","day"]);
