564b65e6f60f000759914ed5a19a7338669fe287
Kanyisa-Ntombini <kanyisa.ntombini@gmail.com>
2022-04-11 16:05:19 +0200

    it iterates the write amount of times

--- task2-3.js
+++ task2-3.js
@@ -1,13 +1,22 @@
-const columns = arr => {
-  let printWord = '';
-  for (let i=0; i<10; i++) {
-    printWord = '';
+const getLongestWordLength = arrWords => {
+  let longestNumber = Number.NEGATIVE_INFINITY;
+
+  for (let word of arrWords) {
+    longestNumber = word.length > longestNumber ? word.length : longestNumber;
+  }
+  return longestNumber;
+}
+
+const columns = (arr, longestWordFunc) => {
+  const longestWordLength = longestWordFunc(arr);
+
+  for (let i=0; i<longestWordLength; i++) {
+    let printWord = '';
     for (let word of arr) {
-      //printWord += word[i];
       printWord = word[i] ? printWord + word[i] + ' ': printWord + '  ';
     }
     console.log(printWord);
   }
 }
 
-columns(["Write","good","code","every","day"]);
+columns(["Write","good","code","every","day"], getLongestWordLength);
--- testing.js
+++ testing.js
@@ -1,25 +1,10 @@
-const triangle = num => {
-  let printHash = '';
-  for (let i=1; i<=num; i++) {
-    //console.log('----------------------');
-    printHash = '';
-    for (let j=1; j<2*num; j++) {
-      if (j >= i && j <= 2*num - i) {
-        printHash += '#';
-        /*console.log('if');
-        console.log(`j=${j}`);
-        console.log(`num-i=${num-i}`);
-        console.log(`2*num-i=${2*num-i}`);*/
-      } else {
-        printHash += 'o';
-        /*console.log('if');
-        console.log(`j=${j}`);
-        console.log(`num-i=${num-i}`);
-        console.log(`2*num-i=${2*num-i}`);*/
-      }
-    }
-    console.log(printHash);
+const longestWord = arrWords => {
+  let longestNumber = Number.NEGATIVE_INFINITY;
+
+  for (let word of arrWords) {
+    longestNumber = word.length > longestNumber ? word.length : longestNumber;
   }
+  return longestNumber;
 }
 
-triangle(4);
+console.log(longestWord(["Write","good","code", 'dfsdgdsfgds',"every","day"]));
